a0cde89760b2a7e953e3befcb407310747531cc8
Shubham <samaroraytb@gmail.com>
2023-03-10 10:09:34 +0000

    Functionality Added

--- src/components/Stopwatch/index.js
+++ src/components/Stopwatch/index.js
@@ -2,7 +2,45 @@ import {Component} from 'react'
 import './index.css'
 
 class Stopwatch extends Component {
+  state = {stateMinutes: 0, stateSeconds: 0, isTimeRunning: false}
+
+  updateUiTime = () => {
+    const {stateSeconds} = this.state
+    if (stateSeconds < 59) {
+      this.setState(prevState => ({stateSeconds: prevState.stateSeconds + 1}))
+    } else {
+      this.setState(prevState => ({stateMinutes: prevState.stateMinutes + 1}))
+      this.setState({stateSeconds: 0})
+    }
+    console.log('Callback Function Called')
+  }
+
+  startButtonClicked = () => {
+    console.log('Start Button Clicked')
+    this.setState(prevState => ({isTimeRunning: !prevState.isTimeRunning}))
+    this.timerId = setInterval(this.updateUiTime, 1000)
+  }
+
+  stopButtonClicked = () => {
+    clearInterval(this.timerId)
+    this.setState(prevState => ({isTimeRunning: !prevState.isTimeRunning}))
+    console.log('Stopped Button Clicked')
+  }
+
+  resetTimerButton = () => {
+    clearInterval(this.timerId)
+    this.setState({stateMinutes: 0, stateSeconds: 0, isTimeRunning: false})
+    console.log('Reset Button Clicked')
+  }
+
   render() {
+    const {stateMinutes, stateSeconds, isTimeRunning} = this.state
+
+    const formattedStateMinutes =
+      stateMinutes < 10 ? `0${stateMinutes}` : stateMinutes
+    const formattedStateSeconds =
+      stateSeconds < 10 ? `0${stateSeconds}` : stateSeconds
+
     return (
       <div className="main-div-bg-container">
         <div className="stop-watch-container-with-heading">
@@ -17,16 +55,31 @@ class Stopwatch extends Component {
               <p>Timer</p>
             </div>
             <div className="time-container">
-              <h1>00:50</h1>
+              <h1>{`${formattedStateMinutes}:${formattedStateSeconds}`}</h1>
             </div>
             <div className="controller-buttons">
-              <button type="button" className="green common-button-style">
+              <button
+                disabled={isTimeRunning}
+                onClick={this.startButtonClicked}
+                type="button"
+                className="green common-button-style"
+              >
                 Start
               </button>
-              <button type="button" className="red common-button-style">
+              <button
+                onClick={this.stopButtonClicked}
+                disabled={!isTimeRunning}
+                type="button"
+                className="red common-button-style"
+              >
                 Stop
               </button>
-              <button type="button" className="orange common-button-style">
+              <button
+                disabled={isTimeRunning}
+                onClick={this.resetTimerButton}
+                type="button"
+                className="orange common-button-style"
+              >
                 Reset
               </button>
             </div>
